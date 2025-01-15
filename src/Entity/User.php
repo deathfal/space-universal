21062c7ecb9b1c2535586c27490b23548a4a3398
deathfal <adamgarchi@gmail.com>
2025-01-15 12:16:12 +0100

    Register + add Mailer

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -152,7 +152,10 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     public function getRoles(): array
     {
-        return $this->roles;
+        $roles = $this->roles;
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
     }
 
     public function setRoles(array $roles): static
