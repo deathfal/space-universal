ba77bfd04d633b69d7ca78783e643935de5d1b6c
Remy Saguez <resaguez.2800@gmail.com>
2025-01-16 11:05:37 +0100

    product page completed

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -11,11 +11,18 @@ class ProductController extends AbstractController
     #[Route('/products', name: 'app_products')]
     public function index(): Response
     {
-        // Exemple de données de produits
+
         $products = [
-            ['id' => 1, 'name' => 'Produit 1', 'price' => '100 UEC', 'image' => 'path/to/image1.jpg', 'rating' => 4.5],
-            ['id' => 2, 'name' => 'Produit 2', 'price' => '200 UEC', 'image' => 'path/to/image2.jpg', 'rating' => 4.0],
-            // Ajoutez plus de produits ici
+            ['id' => 1, 'name' => 'Produit 1', 'price' => '100 UEC', 'image' => 'img/AcryliPlex.jpg', 'rating' => 4.5],
+            ['id' => 2, 'name' => 'Produit 2', 'price' => '200 UEC', 'image' => 'img/AcryliPlex.jpg', 'rating' => 4.0],
+            ['id' => 3, 'name' => 'Produit 3', 'price' => '200 UEC', 'image' => 'img/AcryliPlex.jpg', 'rating' => 4.0],
+            ['id' => 4, 'name' => 'Produit 1', 'price' => '100 UEC', 'image' => 'img/AcryliPlex.jpg', 'rating' => 4.5],
+            ['id' => 5, 'name' => 'Produit 2', 'price' => '200 UEC', 'image' => 'img/AcryliPlex.jpg', 'rating' => 4.0],
+            ['id' => 6, 'name' => 'Produit 3', 'price' => '200 UEC', 'image' => 'img/AcryliPlex.jpg', 'rating' => 4.0],
+            ['id' => 7, 'name' => 'Produit 1', 'price' => '100 UEC', 'image' => 'img/AcryliPlex.jpg', 'rating' => 4.5],
+            ['id' => 8, 'name' => 'Produit 2', 'price' => '200 UEC', 'image' => 'img/AcryliPlex.jpg', 'rating' => 4.0],
+            ['id' => 9, 'name' => 'Produit 3', 'price' => '200 UEC', 'image' => 'img/AcryliPlex.jpg', 'rating' => 4.0],
+
         ];
 
         return $this->render('products/index.html.twig', [
