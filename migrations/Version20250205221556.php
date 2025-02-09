baaec34d193ec5f2dd05ce41a4b6d0eedef3130b
deathfal <adamgarchi@gmail.com>
2025-02-09 21:00:12 +0100

    add doc/bdd + fix cy

--- migrations/Version20250205221556.php
+++ migrations/Version20250205221556.php
@@ -7,9 +7,7 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
+
 final class Version20250205221556 extends AbstractMigration
 {
     public function getDescription(): string
